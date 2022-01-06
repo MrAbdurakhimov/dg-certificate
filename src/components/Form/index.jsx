1a599eb01291b275a3623e815c30715697930f11
Mukhammadyusuf Abdurakhimov <mukhammadyusuf_abdurakhimov@epam.com>
2022-01-07 02:50:32 +0500

    mui, responsive

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import styled from "styled-components";
+import { Button, FormControl, TextField } from "@mui/material";
+const Container = styled.div`
+  padding: 20px 0;
+`;
 
 const Form = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Box
+        component="form"
+        fullWidth
+        sx={{ m: 0 }}
+        noValidate
+        autoComplete="off"
+      >
+        <FormControl fullWidth sx={{ mt: 1, mb: 1 }}>
+          <TextField
+            id="filled-basic"
+            label="Email manizilingiz"
+            variant="filled"
+          />
+        </FormControl>
+        <Button
+          fullWidth
+          sx={{ mt: 1, mb: 1 }}
+          variant="contained"
+          size="large"
+        >
+          Login
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default Form;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { mainBg } from "../../assets/";
+import Form from "../../components/Form/";
+
 const Container = styled.div`
   display: flex;
   height: 100vh;
@@ -8,24 +10,32 @@ const Container = styled.div`
 const Section = styled.div`
   flex: 1;
   display: flex;
+  height: 100vh;
   padding: 64px 24px;
   text-align: center;
   flex-direction: column;
   justify-content: center;
   &:nth-child(2) {
     background: url(${mainBg});
+    padding: 0;
   }
+`;
+const Overlay = styled.div`
+  background: rgba(0, 68, 255, 0.2);
+  flex: 1;
 `;
 
 const Text = styled.div`
-  flex: 1;
+  /* flex: 1; */
   font-family: "Sora", sans-serif;
   font-size: 32px;
 `;
 const Desc = styled.div`
   flex: 1;
+  text-align: left;
   font-family: "Inter", sans-serif;
   font-size: 16px;
+  padding: 20px 0;
 `;
 
 const Login = () => {
@@ -33,9 +43,14 @@ const Login = () => {
     <Container>
       <Section>
         <Text>Login</Text>
-        <Desc>Please provide your login details to get your certificate.</Desc>
+        <Desc>
+          Iltimos sertifikatingizni olish uchun email manzilingizni kiriting.
+          <Form />
+        </Desc>
       </Section>
-      <Section></Section>
+      <Section>
+        <Overlay></Overlay>
+      </Section>
     </Container>
   );
 };
